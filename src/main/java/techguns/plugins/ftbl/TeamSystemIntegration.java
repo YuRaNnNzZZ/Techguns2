93d204613474b25b80734ced24470c15aeb8dd63
YuRaNnNzZZ <medved609@yandex.ru>
2018-10-14 18:09:49 +0300

    dependencies fix, ftblib and forge update

--- src/main/java/techguns/plugins/ftbl/TeamSystemIntegration.java
+++ src/main/java/techguns/plugins/ftbl/TeamSystemIntegration.java
@@ -2,36 +2,35 @@ package techguns.plugins.ftbl;
 
 import java.util.UUID;
 
-import com.feed_the_beast.ftbl.api.FTBLibAPI;
-import com.feed_the_beast.ftbl.api.IForgePlayer;
-import com.feed_the_beast.ftbl.api.IForgeTeam;
-import com.feed_the_beast.ftbl.lib.util.misc.EnumPrivacyLevel;
+import com.feed_the_beast.ftblib.lib.data.ForgePlayer;
+import com.feed_the_beast.ftblib.lib.data.ForgeTeam;
+import com.feed_the_beast.ftblib.lib.data.Universe;
 
 public class TeamSystemIntegration {
 
 	public static boolean isAllied(UUID ply1, UUID ply2) {
-		IForgePlayer p1 = FTBLibAPI.API.getUniverse().getPlayer(ply1);
-		IForgePlayer p2 = FTBLibAPI.API.getUniverse().getPlayer(ply2);
-		
+		ForgePlayer p1 = Universe.get().getPlayer(ply1);
+		ForgePlayer p2 = Universe.get().getPlayer(ply2);
+
 		if(p1!=null) {
-			IForgeTeam t1 = p1.getTeam();
+			ForgeTeam t1 = p1.team;
 			if(t1!=null) {
 				return t1.isMember(p2) || t1.isAlly(p2);
 			} else {
 				return ply1.equals(ply2);
 			}
-			
+
 		} else {
 			return ply1.equals(ply2);
 		}
 	}
 	
 	public static boolean isAlliedNoMember(UUID ply1, UUID ply2) {
-		IForgePlayer p1 = FTBLibAPI.API.getUniverse().getPlayer(ply1);
-		IForgePlayer p2 = FTBLibAPI.API.getUniverse().getPlayer(ply2);
+		ForgePlayer p1 = Universe.get().getPlayer(ply1);
+		ForgePlayer p2 = Universe.get().getPlayer(ply2);
 		
 		if(p1!=null) {
-			IForgeTeam t1 = p1.getTeam();
+			ForgeTeam t1 = p1.team;
 			if(t1!=null) {
 				return t1.isAlly(p2) && !t1.isMember(p2);
 			} else {
@@ -43,11 +42,11 @@ public class TeamSystemIntegration {
 	}
 	
 	public static boolean isTeamMember(UUID ply1, UUID ply2) {
-		IForgePlayer p1 = FTBLibAPI.API.getUniverse().getPlayer(ply1);
-		IForgePlayer p2 = FTBLibAPI.API.getUniverse().getPlayer(ply2);
+		ForgePlayer p1 = Universe.get().getPlayer(ply1);
+		ForgePlayer p2 = Universe.get().getPlayer(ply2);
 		
 		if(p1!=null) {
-			IForgeTeam t1 = p1.getTeam();
+			ForgeTeam t1 = p1.team;
 			if(t1!=null) {
 				return t1.isMember(p2);
 			} else {
@@ -59,11 +58,11 @@ public class TeamSystemIntegration {
 	}
 	
 	public static boolean isEnemy(UUID ply1, UUID ply2) {
-		IForgePlayer p1 = FTBLibAPI.API.getUniverse().getPlayer(ply1);
-		IForgePlayer p2 = FTBLibAPI.API.getUniverse().getPlayer(ply2);
+		ForgePlayer p1 = Universe.get().getPlayer(ply1);
+		ForgePlayer p2 = Universe.get().getPlayer(ply2);
 		
 		if(p1!=null) {
-			IForgeTeam t1 = p1.getTeam();
+			ForgeTeam t1 = p1.team;
 			if(t1!=null) {
 				return t1.isEnemy(p2);
 			} else {
